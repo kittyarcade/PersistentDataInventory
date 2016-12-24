5a3c20e2232c38fa4294e4e61d8179e6ac1376e9
Amanda Jopp <aclaire488@gmail.com>
2016-12-24 11:29:59 -0600

    server running

--- server/app.js
+++ server/app.js
@@ -8,9 +8,26 @@ var port = process.env.PORT || 3000;
 
 var connectionString = 'postgres://localhost:5432/Inventory';
 
+//spin up server
+app.listen(port,function(){
+  console.log('serving listening on: ', port);
+}); //end server spin up
 
+//base url
+app.get('/', function(req, res){
+  console.log('base url hit');
+  res.sendFile(path.resolve('views/index.html'));
+}); //end base hit
 
+//add new item to inventory
+app.post('/addItem', urlEncodedParser, function(req, res){
+  console.log('addItem route hit: ', req.body);
+}); //end add new item to the inventory
 
+//get all objects in the inventory
+app.get('/getInventory', function(req, res){
+  console.log('getInventory route hit');
+}); //end get all objects in the inventory
 
 //static folder
 app.use(express.static('public'));
